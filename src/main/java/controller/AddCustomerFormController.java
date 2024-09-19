1b0f8f84533eb40bb5a04c3cc4e87172bf2a6c84
yuwantha98 <yuwantharavihara123@gmail.com>
2024-09-19 11:38:40 +0530

    Add data to database

--- src/main/java/controller/AddCustomerFormController.java
+++ src/main/java/controller/AddCustomerFormController.java
@@ -13,13 +13,26 @@ import javafx.scene.control.DatePicker;
 import model.Customer;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class AddCustomerFormController implements Initializable {
 
     @FXML
-    public JFXTextField txtNumber;
+    public JFXTextField txtSalary;
+
+    @FXML
+    public JFXTextField txtCity;
+
+    @FXML
+    public JFXTextField txtProvince;
+
+    @FXML
+    public JFXTextField txtPostalCode;
 
     @FXML
     private ComboBox<String> cmbTitle;
@@ -47,42 +60,58 @@ public class AddCustomerFormController implements Initializable {
 
     @FXML
     void btnAddCustomerOnAction(ActionEvent event) {
-        List<Customer> customerList = DBConnection.getInstance().getConnection();
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "12345");
 
-        boolean idExists = customerList.stream()
-                .anyMatch(customer -> customer.getId().equals(txtId.getText()));
+            String insertSQL = "INSERT INTO customer (CustID, CustTitle, CustName, CustAddress, DOB, salary, City, Province, PostalCode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement insertStmt = connection.prepareStatement(insertSQL);
 
-        if (idExists) {
+            insertStmt.setString(1, txtId.getText());
+            insertStmt.setString(2, cmbTitle.getValue());
+            insertStmt.setString(3, txtName.getText());
+            insertStmt.setString(4, txtAddress.getText());
+            insertStmt.setDate(5, java.sql.Date.valueOf(dateDob.getValue()));
+            insertStmt.setDouble(6, Double.parseDouble(txtSalary.getText()));
+            insertStmt.setString(7, txtCity.getText());
+            insertStmt.setString(8, txtProvince.getText());
+            insertStmt.setString(9, txtPostalCode.getText());
 
+            int rowsAffected = insertStmt.executeUpdate();
+
+            if (rowsAffected > 0) {
+
+                txtId.setText("");
+                txtName.setText("");
+                txtAddress.setText("");
+                txtSalary.setText("");
+                txtCity.setText("");
+                txtProvince.setText("");
+                txtPostalCode.setText("");
+                dateDob.setValue(null);
+                cmbTitle.setValue(null);
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Success");
+                alert.setHeaderText(null);
+                alert.setContentText("Customer added successfully!");
+                alert.showAndWait();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
             Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Duplicate ID Error");
+            alert.setTitle("Database Error");
             alert.setHeaderText(null);
-            alert.setContentText("The customer ID already exists. Please enter a unique ID.");
+            alert.setContentText("An error occurred while adding the customer to the database.");
             alert.showAndWait();
-        } else {
-//            customerList.add(
-//                    new Customer(
-//                            txtId.getText(),
-//                            txtName.getText(),
-//                            txtAddress.getText(),
-//                            txtNumber.getText(),
-//                            cmbTitle.getValue(),
-//                            dateDob.getValue()
-//                    )
-//            );
-
-            txtId.setText("");
-            txtName.setText("");
-            txtAddress.setText("");
-            txtNumber.setText("");
-            dateDob.setValue(null);
-
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Success");
+        } catch (NumberFormatException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Input Error");
             alert.setHeaderText(null);
-            alert.setContentText("Customer added successfully!");
+            alert.setContentText("Please ensure all fields are filled out correctly.");
             alert.showAndWait();
         }
     }
+
 
 }
